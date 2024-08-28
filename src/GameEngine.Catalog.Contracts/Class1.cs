9b6031d1b40e057163802e7086aea25441ff5d1d
Richard <nwonahr@gmail.com>
2024-08-28 04:50:46 +0100

    CORS edit

---
+++ src/GameEngine.Catalog.Contracts/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace GameEngine.Catalog.Contracts;
+
+public class Class1
+{
+
+}
--- src/GameEngine.Inventory.Service/Program.cs
+++ src/GameEngine.Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
 using GameEngine.Common.MongoDB;
 using GameEngine.Inventory.Service;
 using GameEngine.Inventory.Service.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,36 @@ builder.Services.AddMongo(config)
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("http://localhost:5253");
-});
+})
+.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(
+    5,
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+    onRetry: (outcome, timeSpan, retryAttempt) =>
+    {
+         var serviceProvider = builder.Services.BuildServiceProvider();  //4:42:17
+        serviceProvider.GetService<ILogger<CatalogClient>()?
+            .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry{retryAttempt}");
+    }
+))
+.AddTransientHttpErrorPolicy(builder => builder.Dr<TimeoutRejectedException>().CircuitBreakerAsync(
+    3,
+    TimeSpan.FromSeconds(15),
+    onBreak: (outcome, timespan) =>
+    {
+       var serviceProvider = ServicesModelBinder.BuildServiceProvider();
+        serviceProvider.GetService<ILogger<CatalogClient>()?
+            .LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+
+    },
+    onReset: () => {
+        var serviceProvider = ServicesModelBinder.BuildServiceProvider();
+        serviceProvider.GetService<ILogger<CatalogClient>()?
+            .LogWarning($"Closing the circuit...");
+
+    }
+))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 // Add services to the container.
 
